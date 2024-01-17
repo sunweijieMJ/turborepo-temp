065b01eb3277b638d27abc709622287a320829bd
weijie.sun <weijie.sun@yitu-inc.com>
2024-01-17 10:44:20 +0800

    fix: basename

--- apps/micro-app/src/router/index.tsx
+++ apps/micro-app/src/router/index.tsx
@@ -3,12 +3,15 @@ import HomePage1 from '../pages/HomePage1';
 import HomePage2 from '../pages/HomePage2';
 
 function Router() {
+  // eslint-disable-next-line no-underscore-dangle
+  const basename = window.__POWERED_BY_QIANKUN__ ? '/micro-app' : '/';
+
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={basename}>
       <Routes>
-        <Route path="/" element={<Navigate to={'/micro-app/HomePage1'} />} />
-        <Route path="/micro-app/HomePage1" element={<HomePage1 />} />
-        <Route path="/micro-app/HomePage2" element={<HomePage2 />} />
+        <Route path="/" element={<Navigate to={'/HomePage1'} />} />
+        <Route path="/HomePage1" element={<HomePage1 />} />
+        <Route path="/HomePage2" element={<HomePage2 />} />
         <Route path="*" element={null} />
       </Routes>
     </BrowserRouter>
